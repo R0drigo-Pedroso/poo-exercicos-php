11227b0fd131993572ca6f4e29451cc62e261c98
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-06-08 11:47:24 -0300

    Update Livros.php

--- src/Livros.php
+++ src/Livros.php
@@ -4,101 +4,31 @@ class Livros {
         private string $autor;
         private int $paginas=0;
 
-
-        
-
-    
-        // public function getTitulo() {
-        //     return $this->titulo;
-        // }
-
-        // public function setTitulo($titulo) {
-        //     $this->titulo = $titulo;
-        // }
-
-        // public function getAutor() {
-        //     return $this->autor;
-        // }
-
-        // public function setAutor($autor) {
-        //     $this->autor = $autor;
-        // }
-
-        // public function getPaginas() {
-        //     return $this->paginas;
-        // }
-
-        // public function setPaginas($paginas) {
-        //     $this->paginas = $paginas;
-        // }
-
-        /**
-         * Get the value of titulo
-         *
-         * @return string
-         */
         public function getTitulo(): string
         {
                 return $this->titulo;
         }
 
-        /**
-         * Set the value of titulo
-         *
-         * @param string $titulo
-         *
-         * @return self
-         */
-        public function setTitulo(string $titulo): self
-        {
+        public function setTitulo(string $titulo):self{
                 $this->titulo = $titulo;
-
                 return $this;
         }
 
-        /**
-         * Get the value of autor
-         *
-         * @return string
-         */
-        public function getAutor(): string
-        {
+        public function getAutor():string {
                 return $this->autor;
         }
-
-        /**
-         * Set the value of autor
-         *
-         * @param string $autor
-         *
-         * @return self
-         */
-        public function setAutor(string $autor): self
-        {
+  
+        public function setAutor(string $autor):self {
                 $this->autor = $autor;
 
                 return $this;
         }
 
-        /**
-         * Get the value of paginas
-         *
-         * @return int
-         */
-        public function getPaginas(): int
-        {
+        public function getPaginas():int {
                 return $this->paginas;
         }
 
-        /**
-         * Set the value of paginas
-         *
-         * @param int $paginas
-         *
-         * @return self
-         */
-        public function setPaginas(int $paginas): self
-        {
+        public function setPaginas(int $paginas):self {
                 $this->paginas = $paginas;
 
                 return $this;
